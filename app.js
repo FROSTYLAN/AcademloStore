6f187ba2fadef7e5fb097f316e73f692876e8ca3
FROSTYLAN <castillo089frosty@gmail.com>
2022-06-01 10:08:24 -0500

    rateLimit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
 // Enable incoming JSON data
 app.use(express.json());
+
+// Limit IP requests
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 1 * 60 * 60 * 1000, // 1 hr
+  message: 'Too many requests from this IP',
+});
+
+app.use(limiter);
+
+// Global error handler
+app.use('*', globalErrorHandler);
 
 module.exports = { app };
